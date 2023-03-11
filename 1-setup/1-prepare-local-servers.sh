61202414914a3896300b9fd1f4eee1a2a79d85bd
renner <renner.alexandre@skyone.solutions>
2023-03-11 20:07:45 -0300

    multipass

--- 1-setup/1-prepare-local-servers.sh
+++ 1-setup/1-prepare-local-servers.sh
@@ -3,23 +3,34 @@
 # Create ssh key if does not exist
 [ ! -f ./ansible/ssh/ansible ] && ssh-keygen -t rsa -N "" -f ./ansible/ssh/ansible 
 
-servers=("LoadBalancer" "webserver1" "webserver2" "db")
+servers=(
+  ["LoadBalancer",  "192.168.64.10"],
+  ["webserver1",  "192.168.64.20"],
+  ["webserver2",  "192.168.64.30"],
+  ["db",  "192.168.64.40"]
+)
+  
 
 # Loop para percorrer a matriz
 for server in "${servers[@]}"
 do
+  server="${server[0]}"
+  ip="${server[1]}"
   # Create instance
   multipass launch 22.04 --name $server
+  multipass stop myinstance
+  multipass set myinstance network.ipv4_address=$ip
+  multipass start myinstance
   
   # create user and set password
   echo -e "1\n1\n" | multipass exec $server -- sudo adduser --gecos "" ansible
   
   # create ssh folder on instance
-multipass  multipass exec LoadBalancer -- sudo mkdir /home/ansible/.ssh/
+  multipass  multipass exec LoadBalancer -- sudo mkdir /home/ansible/.ssh/
 
   # copy pub key
   multipass exec $server -- sudo sh -c "echo '$(cat ./ansible/ssh/ansible.pub)' >> /home/ansible/.ssh/authorized_keys"  
   echo "------"
-  exit
-
 done
+
+multipass list

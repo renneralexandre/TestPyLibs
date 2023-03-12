61468b73ca71f8d975ce46d520c895181b2a5d1b
renner <renner.alexandre@skyone.solutions>
2023-03-12 10:09:18 -0300

    ansible

--- 1-setup/1-prepare-local-servers.sh
+++ 1-setup/1-prepare-local-servers.sh
@@ -7,14 +7,15 @@
 [ ! -f ./ansible/ssh/ansible ] && ssh-keygen -t rsa -N "" -f ./ansible/ssh/ansible 
 chmod 600 ./ansible/ssh/ansible
 
-servers=(
-  "LoadBalancer:192.168.64.2"
-  "webserver1:192.168.64.3"
-  "webserver2:192.168.64.4"
-  "db:192.168.64.5"
-)
-  
+# servers=(
+#   "loadbalancer:192.168.64.2"
+#   "webserver1:192.168.64.3"
+#   "webserver2:192.168.64.4"
+#   "db:192.168.64.5"
+  # "sandbox:192.168.64.6"
 
+# )
+  
 # Loop para percorrer a matriz
 for server in "${servers[@]}"
 do
@@ -31,7 +32,19 @@ do
   
   # create user and set password
   echo -e "1\n1\n" | multipass exec $name -- sudo adduser --gecos "" ansible
-  
+
+  # give sudo privilege to ansible
+  multipass exec $name -- sudo adduser ansible sudo
+
+  # TODO: FIX
+  # does nor work, skipping
+  # multipass exec $name -- sudo echo "ansible ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/ansible
+  # multipass exec $name -- sudo echo "ansible ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/ansible >/dev/null
+  # ssh -i ./ssh/ansible ansible@$name "echo 'ansible ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ansible >/dev/null"
+  # ssh -i ./ssh/ansible ansible@$name "echo -e "1\n" | sudo su - && echo 'ansible ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ansible >/dev/null"
+  # manual insert, login into each instance and running
+  # echo 'ansible ALL=(ALL) NOPASSWD:ALL' | sudo tee /etc/sudoers.d/ansible >/dev/null
+
   # create ssh folder on instance
   multipass exec $name -- sudo mkdir /home/ansible/.ssh/
 
---
+++ ansible/linux-packages-to-test-with-ansible.md
@@ -0,0 +1,26 @@
+iotop
+iftop
+nmon
+ncdu
+iperf
+stress
+ntp
+traceroute
+mtr
+screen
+
+avahi-daemon
+avahi-utils
+cifs-utils
+
+webadmin
+logwatch
+
+acpid
+
+ssl-cert
+tcpdump, ethtool, ldap-utils
+tripwire
+Collectd, mtr, nethogs, tmux
+iptraf-ng, ethtool
+emacs24-nox htop ack-grep openssh-server fail2ban
